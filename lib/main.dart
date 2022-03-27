b300db157bc2924f9592587c8b68106c3d062d03
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-03-27 14:20:41 -0700

    day2 complate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:myapp/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,19 +9,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    
+    
     return MaterialApp(
-      home: Material(
-        
-        child: Center(
-          // ignore: avoid_unnecessary_containers
-          child: Container(
-            child: const Text("30 days of flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
